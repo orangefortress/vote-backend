c0cee4d330525d814459d57ace82cb54324f58b0
orangefortress <orangefortress@proton.me>
2025-08-16 16:25:49 +0200

    Update hello.js

--- api/hello.js
+++ api/hello.js
@@ -1,3 +1,4 @@
-module.exports = (req, res) => {
-  res.send('Hello from root!');
+module.exports = async (req, res) => {
+  res.setHeader("Content-Type", "application/json");
+  res.status(200).json({ ok: true, message: "Hello from Vercel!" });
 };
