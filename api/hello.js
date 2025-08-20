096b30f72cbf0c3c232b06242eb9c180ca1c4880
orangefortress <orangefortress@proton.me>
2025-08-20 18:42:58 +0200

    Update hello.js

--- api/hello.js
+++ api/hello.js
@@ -1,6 +1,6 @@
 // api/hello.js
 module.exports = (req, res) => {
-  // Permissive CORS so fetch() from any site works
+  // Always send permissive CORS
   res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader("Vary", "Origin");
   res.setHeader("Access-Control-Allow-Methods", "GET,OPTIONS");
@@ -8,8 +8,7 @@ module.exports = (req, res) => {
   res.setHeader("Access-Control-Max-Age", "86400");
 
   if (req.method === "OPTIONS") {
-    // No body on preflight
-    res.statusCode = 204;
+    res.statusCode = 204; // No Content
     return res.end();
   }
 
