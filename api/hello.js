0189c933830e829c3da49aa2492f1152333c86a2
orangefortress <orangefortress@proton.me>
2025-08-20 18:03:10 +0200

    Update hello.js

--- api/hello.js
+++ api/hello.js
@@ -1,4 +1,16 @@
 module.exports = async (req, res) => {
+  const origin = req.headers.origin || "";
+  // permissive CORS for hello (simple sanity check endpoint)
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Vary", "Origin");
+  res.setHeader("Access-Control-Allow-Methods", "GET,OPTIONS");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+  res.setHeader("Access-Control-Max-Age", "86400");
+
+  if (req.method === "OPTIONS") {
+    return res.status(200).end();
+  }
+
   res.setHeader("Content-Type", "application/json");
   res.status(200).json({ ok: true, message: "Hello from Vercel!" });
 };
